a77f5d805b63cb04da373f560c902aaa16126fd5
ASHMAN SODHI <ashmansodhi@hotmail.com>
2025-02-06 01:36:42 +0530

    Minor Changes - Version 3 - Assessment 2

--- js/functions.js
+++ js/functions.js
@@ -18,5 +18,34 @@ function ValidateEmailPwd(){
         return;
     }
     
-    window.location.href = "test2.html";
+    window.location.href = "login-success.html";
+}
+
+function ValidateNameEmailPwd(){
+    let name = document.getElementById("name").value.trim();
+    if(name === ""){
+        alert("Name cannot be empty!");
+        return;
+    }
+
+    let email = document.getElementById("email").value.trim();
+    let password = document.getElementById("password").value.trim();
+    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    var passwordFormat = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+
+    if (email === "" || password === "") {
+        alert("Email and Password cannot be empty!");
+        return;
+    }
+
+    if(!mailformat.test(email)){
+        alert("You have entered an invalid email address!");
+        return;
+    }
+    if(!passwordFormat.test(password)){
+        alert("Please enter password according to the format specified!");
+        return;
+    }
+    
+    window.location.href = "reg-success.html";
 }
